38a303a531c3d5c16a2d82b1abd35395666d9532
Shine <stbnlen@gmail.com>
2022-03-15 20:59:11 -0300

    deploy vercel

---
+++ components/layouts/MainLayout.tsx
@@ -0,0 +1,19 @@
+import Head from 'next/head';
+import { Navbar } from '../Navbar';
+import styles from './MainLayout.module.css';
+
+export const MainLayout: React.FC = ({ children }) => {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Home - Crypto</title>
+        <meta name="description" content="Home Page" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <Navbar />
+
+      <main className={styles.main}>{children}</main>
+    </div>
+  );
+};
